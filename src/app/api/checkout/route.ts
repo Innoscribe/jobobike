64887b896ebaeb869d29c31e3793f2858cb7a422
Kashan Khalid <143093682+KashanKhalid123@users.noreply.github.com>
2025-10-31 05:25:32 +0500

    .

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -2,7 +2,7 @@
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-08-27",
+  apiVersion: "2025-08-27.basil",
 });
 
 export async function POST(req: Request) {
